9918c6ff6ff1db28026471186086f12e4ff0f35c
jnoDestiny <yonatandes@gmail.com>
2025-03-01 22:16:52 +0200

    Add lazygit to install.sh

--- install.sh
+++ install.sh
@@ -56,6 +56,14 @@ chmod u+x nvim.appimage
 sudo mkdir -p /opt/nvim
 sudo mv nvim.appimage /opt/nvim/nvim
 
+# Lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+rm lazygit
+rm lazygit.tar.gz
+
 # Nice-to-haves
 sudo apt install net-tools
 sudo apt install pylint
